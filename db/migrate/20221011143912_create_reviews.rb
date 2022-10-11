c8949b2631a26d48be081a2404cee72032e7f0bd
geoffreyogembo <geoffreyogembo06@gmail.com>
2022-10-11 11:30:14 -0400

    associations edit

--- db/migrate/20221011143912_create_reviews.rb
+++ db/migrate/20221011143912_create_reviews.rb
@@ -3,6 +3,8 @@ class CreateReviews < ActiveRecord::Migration[7.0]
     create_table :reviews do |t|
       t.string :textbody
       t.string :rating
+      t.integer :user_id
+      t.integer :recipe_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_143912) do
     t.string "description"
     t.string "ingredients"
     t.string "image_url"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -23,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_143912) do
   create_table "reviews", force: :cascade do |t|
     t.string "textbody"
     t.string "rating"
+    t.integer "user_id"
+    t.integer "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
