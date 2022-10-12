8a7e84729440cef632030b4458cafc23f077b097
gatwi <faith.murithi@student.moringaschool.com>
2022-10-12 12:02:14 +0300

    reviews seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# review data
+Review.create!(rating: 4, textbody: 'I Tested the Most Popular Tuna Steak Recipes and Found the One You Must Make')
+Review.create!(rating: 5 , textbody: 'Yes, the Viral Ice Cream Sandwich Milkshake Recipe Is That Good')
+Review.create!(rating: 4.5, textbody: 'I Tried the Vegetable Rice That Everyone’s Obsessed with and It Definitely Lived Up to the Hype')
+Review.create!(rating: 4.5, textbody: 'If you want to eat a generous burger and do not want to spend so much money just go to Buddies Burger and enjoy the company of its owner, who is very kind and helpful. Thanks for everything!')
+Review.create!(rating: 4.5, textbody: 'I Tried the Stir-Fried Shrimp That Everyone’s Obsessed with and It Definitely Lived Up to the Hype')
