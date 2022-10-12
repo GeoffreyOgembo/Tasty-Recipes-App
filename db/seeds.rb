bad2120fbdf3eb05706cad3554647bba1af4721b
gatwi <faith.murithi@student.moringaschool.com>
2022-10-12 10:25:13 +0300

    serializer update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Review.create!(rating: 4, text_body: 'I Tested the Most Popular Pumpkin Muffin Recipes and Found the One You Must Make')
+Review.create!(rating: 5 , text_body: 'Yes, the Viral Ice Cream Sandwich Milkshake Recipe Is That Good')
+Review.create!(rating: 4.5, text_body: 'I Tried the Lobster Grilled Cheese That Everyone’s Obsessed with and It Definitely Lived Up to the Hype')
