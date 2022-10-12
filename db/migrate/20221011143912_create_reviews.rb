80bceb9a86d987d74b0aa99daa7c3cddda22e282
jackline chepngeno <chepngenojackline0@gmail.com>
2022-10-12 10:51:39 +0300

    recipe model , controller and serializer

--- db/migrate/20221011143912_create_reviews.rb
+++ db/migrate/20221011143912_create_reviews.rb
@@ -3,8 +3,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
     create_table :reviews do |t|
       t.string :textbody
       t.string :rating
-      t.integer :user_id
-      t.integer :recipe_id
+      
 
       t.timestamps
     end
