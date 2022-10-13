6e8181eee896fb23260d576928fc064bba1b69c3
gatwi <faith.murithi@student.moringaschool.com>
2022-10-13 08:45:17 +0300

    fields updated

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+
+
+function Login() {
+  return (
+    <div className="container">
+        <div className="row justify-content-center pt-5">
+            <div className="card-show-lg p-3 mb-5 bg-white rounded">
+            <h3 className="text-center">Log In!</h3>
+            <div className="card-body">
+                <div className="form-group">
+                    <label htmlFor="email" className="form-label pt-2">
+                        Email Address
+                    </label>
+                    <input
+                        autocomplete="off"
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={onChangeHandler}
+                        className="form-control"
+                        id="email"
+                        placeholder="myname@example.com"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email" className="form-label pt-2">
+                        Username
+                    </label>
+                    <input
+                        autocomplete="off"
+                        type="username"
+                        name="username"
+                        value={username}
+                        onChange={onChangeHandler}
+                        className="form-control"
+                        id="username"
+                        placeholder="Xavier"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="region">User Type</label>
+                    <select 
+                        className="form-control" 
+                        id="region"
+                        name="region"
+                        onChange={onChangeHandler}
+                    >
+                        <option value="">Select Your User Type</option>
+                        <option value="nairobi">Admin</option>
+                        <option value="central">Member</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <button 
+                        onClick={submitHandler}
+                        type="button" 
+                        className="btn btn-primary btn-block"
+                    >
+                        Log In
+                    </button>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login;
