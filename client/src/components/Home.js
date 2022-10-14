29b58eb3c05570810d9b733bbd36c5e914bba67d
gatwi <faith.murithi@student.moringaschool.com>
2022-10-14 15:59:23 +0300

    Home carousel

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import Recipe from './Recipe'
 import { useEffect, useState } from 'react'
-import StarRating from './StarRating'
+
 
 
 // const Card = (props) => {
@@ -51,9 +51,31 @@ function Home() {
 
 
   return (
-    <div className='ecipe'>
-    <h1><em>Best Food For Your Belly</em></h1>
-
+    <div>
+      <div className='container'>
+        <p className="card-text">Best Food For Your Belly</p>
+      </div>
+      <div className='container'>
+        <div className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="https://images.themodernproper.com/billowy-turkey/production/posts/Shrimp-Fajitas-12.jpg?w=667&auto=compress%2Cformat&fit=crop&dm=1612803666&s=d42cc5a92e1bd7816c780404e248414a" className="d-block w-80 h-50" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2005%2F06%2F22%2Fnugget-burger-su-1076692-x.jpg&w=272&h=272&c=sc&poi=face&q=60" className="d-block w-80 h-50" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="https://www.bowlofdelicious.com/wp-content/uploads/2015/11/Ahi-Tuna-Steaks-1.jpg" className="d-block w-80 h-50" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="https://images.unsplash.com/photo-1590947132387-155cc02f3212?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" className="d-block w-80 h-50" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="https://media.istockphoto.com/photos/fried-rice-with-ketchup-and-soy-sauce-picture-id1175434591?s=612x612" className="d-block w-80 h-50" alt="..."/>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
